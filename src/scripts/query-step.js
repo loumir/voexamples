c4a1a69ae5911529c9549aa418303a9cf64d9487
Julien ABID <julien.abid@etu.unistra.fr>
2023-05-23 09:18:45 +0200

    handling multiple query steps in one page

--- src/scripts/query-step.js
+++ src/scripts/query-step.js
@@ -1,30 +1,34 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const stepQuery = document.querySelector(".stepquery");
-    const prevBtn = stepQuery.querySelector("#prev-btn");
-    const nextBtn = stepQuery.querySelector("#next-btn");
-    const steps = stepQuery.querySelectorAll(".step");
-    let currentStep = 0;
+    const stepQuery = document.querySelectorAll(".stepquery");
 
-    function showStep(step) {
-        steps[currentStep].classList.remove("active");
-        steps[step].classList.add("active");
+    for (let i = 0; i < stepQuery.length; i++) {
+        function showStep(step) {
+            steps[currentStep].classList.remove("active");
+            steps[step].classList.add("active");
 
-        prevBtn.disabled = step === 0;
+            prevBtn.disabled = step === 0;
 
-        nextBtn.disabled = step === steps.length - 1;
+            nextBtn.disabled = step === steps.length - 1;
 
-        currentStep = step;
+            currentStep = step;
+        }
+
+        const prevBtn = stepQuery[i].querySelector("#prev-btn");
+        const nextBtn = stepQuery[i].querySelector("#next-btn");
+        const steps = stepQuery[i].querySelectorAll(".step");
+        let currentStep = 0;
+
+        prevBtn.addEventListener("click", () => {
+            showStep(currentStep - 1);
+        });
+
+        nextBtn.addEventListener("click", () => {
+            if (currentStep !== steps.length - 1) {
+                showStep(currentStep + 1);
+            }
+        });
+
+        showStep(currentStep);
     }
 
-    prevBtn.addEventListener("click", () => {
-        showStep(currentStep - 1);
-    });
-
-    nextBtn.addEventListener("click", () => {
-        if (currentStep !== steps.length - 1) {
-            showStep(currentStep + 1);
-        }
-    });
-
-    showStep(currentStep);
 });
